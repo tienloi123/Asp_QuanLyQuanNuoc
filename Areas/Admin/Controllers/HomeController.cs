f7c518360df55238c7fe9733f063c9a0132fa1fb
tienloi123 <doantienloi123@gmail.com>
2023-12-08 23:22:51 +0700

    Asp_QuanLyQuanNuoc

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin,Thu ngân")]
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
